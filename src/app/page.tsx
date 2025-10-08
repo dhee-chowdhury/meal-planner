5f29318b192f63bc321afdf12efe2fdb2e5c04db
dhee-chowdhury <dhee.chowdhury@gmail.com>
2025-10-09 02:18:04 +0600

    add db.ts file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
-  return <div>Hello World!</div>;
+import db from "@/lib/db";
+
+export default async function Home() {
+  const user = await db.user.findFirst();
+  return <div>{JSON.stringify(user, null, 2)}</div>;
 }
