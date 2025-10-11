c7d0884d037e8a59154643a30fed51097350b73f
Dhee Chowdhury <dhee.chowdhury@gmail.com>
2025-10-12 04:40:54 +0600

    add tanstack query and write a basic categories fetcher

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,21 +1,26 @@
 "use client";
 import { ThemeProvider as NextThemeProvider } from "next-themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
 type ProviderProps = {
   children: ReactNode;
 };
 
+const queryClient = new QueryClient();
+
 const Providers = ({ children }: ProviderProps) => {
   return (
-    <NextThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </NextThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </NextThemeProvider>
+    </QueryClientProvider>
   );
 };
 
