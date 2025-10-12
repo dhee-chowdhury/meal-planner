4fa67306da79141f53072bde31f4558f3fdbfeca
Dhee Chowdhury <dhee.chowdhury@gmail.com>
2025-10-12 09:59:53 +0600

    add invalidate querykey on successfull deletion of a category

--- src/app/(dashboard)/admin/foods-management/categories/_services/use-category-mutations.ts
+++ src/app/(dashboard)/admin/foods-management/categories/_services/use-category-mutations.ts
@@ -1,13 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCategory } from "./categoryMutations";
 
 const useDeleteCategory = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async (id: number) => {
       await deleteCategory(id);
     },
     onSuccess: () => {
       // TODO: show a success toast
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
     },
   });
 };
