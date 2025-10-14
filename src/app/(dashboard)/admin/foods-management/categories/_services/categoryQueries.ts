d2685ec0d18c9256eb3551ad7656f1301643af76
Dhee Chowdhury <dhee.chowdhury@gmail.com>
2025-10-14 14:52:40 +0600

    add get category query

--- src/app/(dashboard)/admin/foods-management/categories/_services/categoryQueries.ts
+++ src/app/(dashboard)/admin/foods-management/categories/_services/categoryQueries.ts
@@ -1,9 +1,23 @@
 "use server";
 
 import db from "@/lib/db";
+import { Category } from "../_types/categorySchema";
 
 const getCategories = async () => {
   return await db.category.findMany();
 };
 
-export { getCategories };
+const getCategory = async (id: number): Promise<Category> => {
+  const response = await db.category.findFirst({
+    where: {
+      id: id,
+    },
+  });
+  return {
+    action: "update",
+    name: response?.name ?? "",
+    id,
+  };
+};
+
+export { getCategories, getCategory };
