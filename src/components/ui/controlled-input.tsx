5714af9e02ca17277fbedd4b5667f5c9a1420542
Dhee Chowdhury <dhee.chowdhury@gmail.com>
2025-10-14 14:16:15 +0600

    add controlled input component on top of input

---
+++ src/components/ui/controlled-input.tsx
@@ -0,0 +1,53 @@
+import { cn } from "@/lib/utils";
+import { ComponentProps } from "react";
+import { Controller, FieldValues, Path, useFormContext } from "react-hook-form";
+import { Label } from "./label";
+import { Input } from "./input";
+
+type ControlledInputProps<T extends FieldValues> = {
+  name: Path<T>;
+  label?: string;
+  containerClassName?: string;
+} & ComponentProps<"input">;
+
+const ControlledInput = <T extends FieldValues>({
+  className,
+  name,
+  type,
+  label,
+  containerClassName,
+  ...props
+}: ControlledInputProps<T>) => {
+  const { control } = useFormContext<T>();
+
+  return (
+    <div className={cn("w-full", containerClassName)}>
+      {!!label && (
+        <Label className="mb-2" htmlFor={name}>
+          {label}
+        </Label>
+      )}
+      <Controller
+        name={name}
+        control={control}
+        render={({ field, fieldState: { error }, ...rest }) => (
+          <>
+            <Input
+              {...field}
+              type={type}
+              id={name}
+              data-slot="input"
+              aria-invalid={!!error}
+              className={className}
+              {...rest}
+              {...props}
+            />
+            {!!error && <p className="text-destructive text-sm">{error.message}</p>}
+          </>
+        )}
+      />
+    </div>
+  );
+};
+
+export { ControlledInput };
